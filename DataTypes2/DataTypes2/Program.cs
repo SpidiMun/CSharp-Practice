2db979a9e22d390fa365e8a905cf942789ad68d8
SpidiMun <78678768+SpidiMun@users.noreply.github.com>
2023-07-25 01:02:17 +0500

    Data types №2
    Work with different data types №2

---
+++ DataTypes2/DataTypes2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DataTypes2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string row = "y";
+            while (row == "y")
+            {
+                Console.WriteLine("Введите значение x: ");
+                double x;
+                while (!double.TryParse(Console.ReadLine(), out x))
+                {
+                    Console.WriteLine("Ошибка при введении числа! Введите новое значение x: ");
+                }
+                Console.WriteLine("Введите значение y: ");
+                double y;
+                while (!double.TryParse(Console.ReadLine(), out y))
+                {
+                    Console.WriteLine("Ошибка при введении числа! Введите новое значение y: ");
+                }
+                bool isBeInArea = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2)) <= 1 && y <= 1 - x && y >= -1 - x;
+                Console.WriteLine($"Точка A с координатами ({x}; {y}) принадлежит области допустимых значений? {isBeInArea}");
+                Console.WriteLine("Хотите продолжить!\ty/n");
+                row = Console.ReadLine();
+                while (row != "y" && row != "n")
+                {
+                    Console.WriteLine("Ошибка! Хотите продолжить!\ty/n");
+                    row = Console.ReadLine();
+                }
+            }
+        }
+    }
+}
